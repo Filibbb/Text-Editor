fdcfefc0230209aa79c9ca8987d543d1388a3ae9
Patric <fupat002@student.zhaw.ch>
2021-10-30 16:05:09 +0200

    RegEx adjusted

--- src/main/java/ConsoleInputReader.java
+++ src/main/java/ConsoleInputReader.java
@@ -1,57 +1,23 @@
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
 
 public class ConsoleInputReader {
 
     private final Scanner inputReader;
-    private Set<String> allowedTextElements = new HashSet<>();
-
-    //Ein HashSet machen, dass nur erlaubte zeichen enthält.
+    private static final String notAllowedTextElements = "[^a-zA-Z0-9.,:;!?’()\"%@+*\\-\\[\\]{}/&#$]";
 
     public ConsoleInputReader() {
         inputReader = new Scanner(System.in);
-        addAllowedTextelements();
-    }
-
-    private void addAllowedTextelements() {
-        allowedTextElements.add("[a-zA-Z]");//Letters
-        allowedTextElements.add("[0-9]");//digits
-        allowedTextElements.add(".");
-        allowedTextElements.add(",");
-        allowedTextElements.add(":");
-        allowedTextElements.add(";");
-        allowedTextElements.add("-");
-        allowedTextElements.add("!");
-        allowedTextElements.add("?");
-        allowedTextElements.add("’");
-        allowedTextElements.add("(");
-        allowedTextElements.add(")");
-        allowedTextElements.add("\"");
-        allowedTextElements.add("%");
-        allowedTextElements.add("@");
-        allowedTextElements.add("+");
-        allowedTextElements.add("*");
-        allowedTextElements.add("[");
-        allowedTextElements.add("]");
-        allowedTextElements.add("{");
-        allowedTextElements.add("}");
-        allowedTextElements.add("/");
-        allowedTextElements.add("&");
-        allowedTextElements.add("#");
-        allowedTextElements.add("$");
     }
 
     public String stringInputReader() {
         String userTextInput = inputReader.nextLine();
 
-        for (String allowedTextElement : allowedTextElements) {
-            if (!userTextInput.contains(allowedTextElement)) {
-                System.err.println("Your text doesn't just contain letters, numbers, spaces or punctuation marks such as .,:;-!?’()\"%@+*[]{}/&#$");
-                return stringInputReader();
-            }
+        if (userTextInput.matches(notAllowedTextElements)) {
+            System.err.println("Your text doesn't just contain letters, numbers, spaces or punctuation marks such as .,:;-!?’()\"%@+*[]{}/&#$");
+            return stringInputReader();
         }
-        return userTextInput;
+
+        return userTextInput.trim();
     }
 
 
