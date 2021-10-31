9614132b37a12a637b18a88590eaea342e314f5b
Patric <fupat002@student.zhaw.ch>
2021-10-31 11:39:23 +0100

    numberInputReader updated

--- src/main/java/ConsoleInputReader.java
+++ src/main/java/ConsoleInputReader.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.regex.*;
 
 public class ConsoleInputReader {
 
@@ -11,23 +12,27 @@ public class ConsoleInputReader {
 
     public String stringInputReader() {
         String userTextInput = inputReader.nextLine();
-
         if (userTextInput.matches(notAllowedTextElements)) {
             System.err.println("Your text doesn't just contain letters, numbers, spaces or punctuation marks such as .,:;-!?’()\"%@+*[]{}/&#$");
             return stringInputReader();
-        }else{
+        } else {
             return userTextInput.trim();
         }
     }
 
-    public int numberInputReader(){
-        int userNumberInput = inputReader.nextInt();
-
-        if(userNumberInput < 1){
-            System.err.println("Type in a number greater than one");
+    public int numberInputReader() {
+        if (inputReader.hasNextInt()) {
+            int userNumberInput = inputReader.nextInt();
+            if (userNumberInput < 1) {
+                System.err.println("Type in a number greater than one");
+                return numberInputReader();
+            } else {
+                return userNumberInput;
+            }
+        } else {
+            System.err.println("Type in a number");
+            inputReader.next();
             return numberInputReader();
-        }else {
-            return userNumberInput;
         }
     }
 
