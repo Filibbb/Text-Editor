560f41147a8d4ea032e634f0373dc48579915555
Patric <fupat002@student.zhaw.ch>
2021-11-03 09:07:35 +0100

    replace methods

--- src/main/java/TextData.java
+++ src/main/java/TextData.java
@@ -40,4 +40,39 @@ public class TextData {
     private boolean isValidParagraph(Integer paragraphNumber) {
         return paragraphNumber != null && (paragraphNumber > 0 || paragraphNumber < paragraphs.size());
     }
+
+    /**
+     *Replaces a word in the last paragraph
+     * @param textToReplaced the text that has to be replaced
+     * @param newText the text that replaces the old text
+     * @author fupat002
+     */
+    public void replaceAtLastParagraph(String textToReplaced, String newText){
+        int numberOfLastParagraph = convertParagraphToIndex(paragraphs.size());
+        if(containsWordAtParagraph(textToReplaced, numberOfLastParagraph)){
+            paragraphs.set(numberOfLastParagraph, newText);
+        }else{
+            System.err.println("Your replacement word is not in this line. Check it out and try again.");
+        }
+    }
+
+    /**
+     *Replaces a word in the chosen paragraph
+     * @param textToReplace the text that has to be replaced
+     * @param paragraph the paragraph that contains the word that has to be replaced
+     * @param newText the text that replaces the old text
+     * @author fupat002
+     */
+    public void replaceAtVariableParagraph(String textToReplace, int paragraph ,String newText){
+        if(containsWordAtParagraph(textToReplace, paragraph)){
+            paragraphs.set(paragraph, newText);
+        }else{
+            System.err.println("Your replacement word is not in this line. Check it out and try again.");
+        }
+    }
+
+    private boolean containsWordAtParagraph(String word, int paragraph){
+        String paragraphText = paragraphs.get(paragraph);
+        return paragraphText.contains(word);
+    }
 }
